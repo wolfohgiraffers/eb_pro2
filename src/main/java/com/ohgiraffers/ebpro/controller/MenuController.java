a08f24e3a0ba3b0a8c1e9941451a567a4184b8dd
wolfohgiraffers <wolf.ohgiraffers@gmail.com>
2025-04-30 12:52:24 +0900

    change yml3

--- src/main/java/com/ohgiraffers/ebpro/controller/MenuController.java
+++ src/main/java/com/ohgiraffers/ebpro/controller/MenuController.java
@@ -19,7 +19,7 @@ public class MenuController {
 
     @GetMapping("health")
     public String healthCheck() {
-        return "eb_proj is fine! dev/feature/cr!!!123";
+        return "eb_proj is fine! dev/feature/cr!!!111";
     }
 
     @GetMapping("menus/{menuCode}")
